22d09f26a11d08bb90f08e10e65fd484283e7bda
youssef nagib <youssefnagib14@yahoo.com>
2024-05-18 13:33:36 +0300

    READ_ME

---
+++ README.md
@@ -0,0 +1,24 @@
+#AirBnB clone - The console :
+The AirBnB clone project with a goal of deploying a clone of the AirBnB website. lets go :) :--
+
+##Table of contents :-
+Description.
+Installation.
+Files.
+Contributors.
+#Description :-
+The console is the most important part of the AirBnB project. Goal of project is to deploy the server a simple copy of the AirBnB Website.
+
+First we make command interpreter for managing clone objects :-
+
+1- Create 2- Retrieve 3- Operate 4- Update 5- Destroy
+
+Installation :-
+Clone the repo: git clone "https://github.com/osamablal/AirBnB_clone.git"
+Access Air-Bnb dir : cd AirBnB_clone
+Run hbnb interactive : ./console and enter command
+Run hbnb non interactive : echo "<command>" | ./console.py
+Files :-
+/models    test_base_model_dict.py  test_save_reload_base_model.py  test_base_model.py   AUTHORS   README.md   console.py
+#Contributors :-
+1-Youssef Nagib 2-Youssef yahia
--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import shlex
 import models
 """console.py  that contains the entry point of the command interprete"""
 
+
 class HBNBCommand(cmd.Cmd):
     """The command line """
     prmt = "(hbnb) "
@@ -28,6 +29,10 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        """
         arg = shlex.split(arg)
         if len(arg) == 0:
             print("** class name missing **")
@@ -39,6 +44,10 @@ class HBNBCommand(cmd.Cmd):
             print(creates__new_instance.id)
 
     def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
         arg = shlex.split(arg)
         if len(arg) == 0:
             print("** class name missing **")
@@ -55,6 +64,10 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_destroy(self, arg):
+        """
+        Deletes an instance based on
+        the class name and id
+        """
         arg = shlex.split(arg)
         if len(arg) == 0:
             print("** class name missing **")
@@ -73,6 +86,47 @@ class HBNBCommand(cmd.Cmd):
                     models.storage.save()
                     return
             print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+           Prints all string representation
+           of all instances based or not on the class name
+        """
+        arg = shlex.split(arg)
+        all = models.storage.all()
+        new_list = []
+        if len(arg) == 0:
+            for value in all.values():
+                new_list.append(str(value))
+            print(new_list)
+        elif arg[0] not in self.__class_names:
+            print("** class doesn't exist **")
+        else:
+            for value in all.values():
+                if value.__class__.__name__ == arg[0]:
+                    new_list.append(str(value))
+            print(new_list)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name
+        and id by adding or updating attribute
+        """
+        opject = models.storage.all()
+        arg = arg.split(" ")
+
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.__class_names:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif len(arg) == 2:
+            print("** attribute name missing **")
+        elif len(arg) == 3:
+            print("** value missing **")
+        else:
+            pass
 
 
 if __name__ == '__main__':
